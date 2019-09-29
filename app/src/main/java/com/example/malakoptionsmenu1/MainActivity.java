a3c5cc8654a9b1b131d3cc57fde1a2a52ef058df
Owner <alsalamsce>
2019-09-29 08:55:10 +0300

    malak

--- app/src/main/java/com/example/malakoptionsmenu1/MainActivity.java
+++ app/src/main/java/com/example/malakoptionsmenu1/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.malakoptionsmenu1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +19,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { // create option menu
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.my_menu,menu);
         return true;
     }
-}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {// daly gahzh
+        if (item.getItemId()==R.id.itmScreen1)
+        {
+            Intent intent=new Intent(this,ActivtyScreen1.class);
+            startActivity(intent);
+        }
+        if (item.getItemId()==R.id.itmScreen2)
+        {
+            Intent intent=new Intent(this,ActivtyScreen1.class);
+            startActivity(intent);
+        }
+        if (item.getItemId()==R.id.itemYnet)
+        {
+            Toast.makeText(this,"ynet",Toast.LENGTH_SHORT).show();
+        }
+        return true;//
+
+
+    }
+}
